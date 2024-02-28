12321986ffc766fe723b19053d7649e6930014c6
eubash <nemezidaxxx@mail.ru>
2024-02-28 19:33:09 +0100

    Setup prettier

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,7 @@
-import { sum } from './sum';
+import { sum } from "./sum";
 
-describe('Sum', () => {
-  it('return sum of two numbers', () => {
+describe("Sum", () => {
+  it("return sum of two numbers", () => {
     expect(sum(1, 2)).toEqual(3);
   });
 });
